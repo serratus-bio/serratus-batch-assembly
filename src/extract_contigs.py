decd61fd262b836badfd23db8bfeb68081284c17
rayan <rayan.chikhi@pasteur.fr>
2020-06-18 19:56:12 +0200

    tweak

--- src/extract_contigs.py
+++ src/extract_contigs.py
@@ -8,7 +8,9 @@ g = open(sys.argv[2],"w")
 for line in sys.stdin:
     ctg = line.split()[0]
     ctglen = line.split()[1]
-    print(sys.argv[1],"found contig (len=",ctglen,"=",len(f[ctg]),")")
+    print(sys.argv[1],"found corona contig ID:",ctg)
+    if ctglen != len(f[ctg]):
+        print("len mismatch?!",ctglen,len(f[ctg]))
     seq = f[ctg]
     g.write(">"+seq.long_name+"\n"+str(seq)+"\n")
 g.close()
